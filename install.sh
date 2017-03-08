ee5cea787a3f37792a763e761b5b12f4daf26d4c
Nicholas Paun <nicholas.paun@mail.mcgill.ca>
2017-03-07 19:00:11 -0500

    Fixed up documentation.

--- install.sh
+++ install.sh
@@ -11,10 +11,13 @@ else
 	echo "Congrats. Your PATH will work for the installation"
 fi
 
+echo "Your original extension was backed up to $HOME/.Ionide-BKUP"
+mv ~/.vscode/extensions/Ionide.Ionide-fsharp*/ ~/.Ionide-BKUP
 
-mv ~/.vscode/extensions/Ionide.Ionide-fsharp* ~/.Ionide-BKUP
-tar -xzvf ionide-source.tar.gz -C ~/.vscode/extensions/
+echo "Extracting extension..."
+tar -xzf ionide-source.tar.gz -C ~/.vscode/extensions/
 cp -a Fsi.js ~/.vscode/extensions/Ionide.Ionide-fsharp-2.22.1/Components/
 
+echo ""
 echo "Installation completed."
 echo "A friendly reminded to disable auto-updates or stuff will break again."
