5361bc968de5be50406be78103d6b4c7f0bfa8f9
Nicholas Paun <nicholas.paun@mail.mcgill.ca>
2017-03-07 19:16:28 -0500

    Just in case.

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ which fsiw > /dev/null
 if [ $? != 0 ]; then
 	echo "We need to add $HOME/bin to your path (editing your .profile and hoping that works)"
 	echo "export PATH=\"\$PATH:$HOME/bin\" # http://github.com/nicholaspaun/vsc-sharpener" >> ~/.profile
-	chmod +x ~/.profile
+	chmod u+x ~/.profile
 else
 	echo "Congrats. Your PATH will work for the installation"
 fi
